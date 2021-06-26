01bbe6322bafb232df1439b853f1b28e9776ecbf
Ashish Sheelavantar <ashish.sheelavantar@anz.com>
2021-06-26 22:29:42 +0530

    Adding SAML security integration code for basic end points

---
+++ src/main/java/basics/learn/samlsecurity/SamlSecurityApplication.java
@@ -0,0 +1,13 @@
+package basics.learn.samlsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basics/learn/samlsecurity/SamlSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package basics.learn.samlsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
